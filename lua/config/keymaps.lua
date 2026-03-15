da0f0ccccc1f68f5dfd8ee96080bb50f152f8d8c
sunny <sunnyjuly61993@gmail.com>
2026-03-15 22:43:20 +0900

    Keymaps shortcut added.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,11 @@
-
+-- Basic NVIM keymaps
 vim.keymap.set('n', '<C-]>', ':Explore<CR>;')
 vim.keymap.set('n', '<C-s>', ':w!<CR>')
 vim.keymap.set('n', '<C-q>', ':q!<CR>')
 
-
-
 -- Neotree shortcut
 vim.keymap.set('n', '<leader>l',  ':Neotree filesystem reveal left<CR>')
 vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal top<CR>')
-
 
 -- Keymap for the split pane
 vim.keymap.set('n', '<C-v>', ':vsplit<CR>')
@@ -20,10 +17,10 @@ vim.keymap.set('n', '<F5>', ':ToggleTerm <CR>')
 -- nvim Navbuddy
 vim.keymap.set('n', '<F4>', ':Navbuddy <CR>')
 vim.keymap.set('n', '<F9>', ':Flote <CR>')
+
 -- Neotree shortcut
 vim.keymap.set('n', '<leader>l',  ':Neotree filesystem reveal left<CR>')
 vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal top<CR>')
-
 
 -- Keymap for the split pane
 vim.keymap.set('n', '<C-v>', ':vsplit<CR>')
@@ -34,4 +31,9 @@ vim.keymap.set('n', '<F5>', ':ToggleTerm <CR>')
 
 -- nvim Navbuddy
 vim.keymap.set('n', '<F4>', ':Navbuddy <CR>')
+
+-- Note MD file
 vim.keymap.set('n', '<F9>', ':Flote <CR>')
+
+-- Keymap list
+vim.keymap.set('n', '<F6>', ':FloatermNew nvim ~/Desktop/Notes/keymap.md <CR>')
